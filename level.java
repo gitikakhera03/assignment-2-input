f6bdaad0bfcafbf5442f3668624e2efcd7827eb5
gitikakhera03 <92471993+gitikakhera03@users.noreply.github.com>
2023-04-03 16:28:16 +0530

    Add files via upload

---
+++ level.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class level {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String First = sc.nextLine();
+        String last = sc.nextLine();
+        System.out.println(First + last);
+    }
+
+}
